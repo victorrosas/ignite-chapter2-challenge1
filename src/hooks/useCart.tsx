27e4754e93d545730571d404cbe423081d9498ea
Victor Rosas <victor.rosas.96@hotmail.com>
2021-04-08 16:21:47 -0500

    finished home component test

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -69,9 +69,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      TODO
+      // TODO
     } catch {
-      TODO
+      // TODO
     }
   };
 
@@ -80,9 +80,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      TODO
+      // TODO
     } catch {
-      TODO
+      // TODO
     }
   };
 
